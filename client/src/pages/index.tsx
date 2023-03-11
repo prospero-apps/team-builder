54783e3c701cc18427c94065d8531117c23c691f
Kamil <prospero.apps@gmail.com>
2023-03-11 14:36:53 +0100

    Generate the Prisma Client, then create a basic React component with a Sign In button and wrap the entire app in a SessionProvider

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { Inter } from 'next/font/google'
+import { signIn, useSession } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data } = useSession()  
+
   return (
-    <div>Hello</div>
+    <div>
+      <button onClick={() => signIn('google')}>Sign in with Google</button>
+      {data?.user?.name}
+    </div>
   )
 }
