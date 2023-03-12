d6f7a170a243c246bc31c6cee13f676c44bc11e7
Kamil <prospero.apps@gmail.com>
2023-03-12 15:10:39 +0100

    Install Chakra UI, wrap the application in the ChakraProvider and define the theme

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   )
 }
