d96314305a5865420177f185a91ef5fa03eca584
Kamil <prospero.apps@gmail.com>
2023-03-13 19:00:32 +0100

    Set up Apollo GraphQL Client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,13 +2,17 @@ import { theme } from '../chakra/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../graphql/apollo-client'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionProvider>
+    </ApolloProvider>
   )
 }
